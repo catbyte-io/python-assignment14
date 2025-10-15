dc575e1f01a67f600b230a962bd45ae49d61597a
catbyte-io <catbyte.io@gmail.com>
2025-10-14 21:08:32 -0600

    Use XPATH to locate child div

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -12,22 +12,40 @@ try:
 
     table_results = []
 
+    # Find table with the year links
     table = driver.find_element(By.CLASS_NAME, 'ba-table')
 
+    # Find all the link elements in the table
     table_links = table.find_elements(By.CSS_SELECTOR, 'a')
 
+
+    # Access each link and click
     for link in table_links:
         values = {}
         values['title'] = link.get_attribute('title')
         values['link'] = link.get_attribute('href')
 
-        # Click on link and save data
+        print(values['link'])
 
-           
+        table_results.append(values)
 
+
+    for value in table_results:
+
+        try:
+            driver.get(value['link'])
+
+            wrapper = driver.find_element(By.ID, 'wrapper')
+            print(wrapper.get_attribute('id'))
+
+            container = wrapper.find_element(By.XPATH, 'child::div[2]')
+            print(container.get_attribute('class'))
+
+            time.sleep(10)
+        except Exception as e:
+            print(f"Exception: {type(e).__name__} {e}")
 
 except Exception as e:
-    print("Could not find website.")
     print(f"Exception: {type(e).__name__} {e}")
 finally:
     driver.quit()
