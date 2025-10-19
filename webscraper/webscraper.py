9ba8d280d3f5fc3f4e2d42d49d54b97880019da6
catbyte-io <catbyte.io@gmail.com>
2025-10-19 11:58:10 -0600

    Remove headers code and DataFrame code

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -4,8 +4,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 import re
+import csv
 import time
-import pandas as pd
+
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
@@ -38,7 +39,6 @@ try:
 
     # Go through results and make a new get request for each link
     for value in table_results:
-
         try:
             driver.get(value['link'])
 
@@ -59,35 +59,21 @@ try:
             # Get the leaderboard tables of data
             ba_tables = container.find_elements(By.CLASS_NAME, 'ba-table')
 
-            # Get the data for hitting statistics leaderboard
+            # Get the data for hitting and pitching statistics leaderboards
             data_tables = ba_tables[0:2]
 
-            print("Data Tables")
-            print(data_tables)
-
+            # For both leaderboard tables, perform the following:
             for table in data_tables:
+
+                # Find all table row elements
                 data_rows = table.find_elements(By.CSS_SELECTOR, 'tr')
-                print(len(data_rows))
-
-                data_headers = []
-
-                headers = table.find_elements(By.CLASS_NAME, 'banner')
-                for header in headers:
-                    header_text = header.text
-                    data_headers.append(header_text)
-            
-                data_headers = data_headers[:len(data_headers)//2]
-
-                print("Headers:")
-                print(data_headers)
-
-                print(len(data_rows))
-
 
                 for each in data_rows[2:-2]:  # Skips the title and header rows
-
+                    
+                    # List to save each td text
                     columns = []
                     
+                    # Find all table data elements for this row
                     td_elements = each.find_elements(By.CSS_SELECTOR, 'td')
 
                     for one in td_elements:
@@ -96,6 +82,7 @@ try:
 
                     print(columns)
 
+                    # Create a dict that is a row of data
                     row = {}
 
                     row['league'] = league
@@ -104,25 +91,22 @@ try:
                     row['name'] = columns[1]
                     row['team'] = columns[2]
                     row['value'] = columns[3]
-
-                        
+                    
+                    # Add each row to the baseball_stats dataset
                     baseball_stats.append(row)
 
 
             # Prevent rapid requests
             time.sleep(10)
 
+
         except Exception as e:
             print(f"Exception: {type(e).__name__} {e}")
 
 
-    # Create DataFrame for hitting data leaderboard
-    baseball_stats_df = pd.DataFrame(baseball_stats)
-    baseball_stats_df.info
-
-
 except Exception as e:
     print(f"Exception: {type(e).__name__} {e}")
+
 finally:
-    driver.quit()
+    driver.quit()  # Close driver
  
