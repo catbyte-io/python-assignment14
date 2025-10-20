9c7247e45edfe4575a82b80a5accfda4f4553ddf
catbyte-io <catbyte.io@gmail.com>
2025-10-20 15:49:25 -0600

    Add wait support, fix typos, and clean data

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import re
 import io
@@ -15,8 +17,11 @@ def main():
     try:
         driver.get('https://www.baseball-almanac.com/yearmenu.shtml')
 
+        # Use WebDriverWait for elements
+        wait = WebDriverWait(driver, 10)
+
         # Create headers
-        headers = ['League', 'Year', 'Type', 'Statistc', 'Name(s)', 'Team(s)', '#', 'Top 25']
+        headers = ['League', 'Year', 'Type', 'Statistic', 'Name(s)', 'Team(s)', '#', 'Top 25']
 
         # Create baseball DataFrame
         baseball_df = pd.DataFrame(columns=headers)
@@ -25,92 +30,118 @@ def main():
         baseball_stats = []
 
         # Find table with the year links
-        table = driver.find_element(By.CLASS_NAME, 'ba-table')
+        table = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'ba-table')))
 
-        # Find all the link elements in the table
-        table_links = table.find_elements(By.CSS_SELECTOR, 'a')
+        if table:
+            # Find all the link elements in the table
+            table_links = table.find_elements(By.CSS_SELECTOR, 'a')
 
-        # List to save table results
-        table_results = []
+            # List to save table results
+            table_results = []
 
-        # Access each link and click
-        for link in table_links:
-            values = {}
-            values['title'] = link.get_attribute('title')
-            values['link'] = link.get_attribute('href')
+            # Access each link and click
+            for link in table_links:
+                values = {}
+                values['title'] = link.get_attribute('title')
+                values['link'] = link.get_attribute('href')
 
-            # print(values['link'])
+                # print(values['link'])
 
-            table_results.append(values)
-
-
-        # Go through results and make a new get request for each link
-        for value in table_results:
-            try:
-                driver.get(value['link'])
-
-                # Get the league
-                league = value['title'].split()[-2:]
-                league = ' '.join(league)
-
-                # Get the year
-                match = re.search(r'(\d{4})', value['title'])
-                year = match.group(1)
-
-                # Get the wrapper element that holds the div
-                wrapper = driver.find_element(By.ID, 'wrapper')
-
-                # Get the container div that holds the statistic data tables 
-                container = wrapper.find_element(By.XPATH, 'child::div[2]')
-                
-                # Get the leaderboard tables of data
-                ba_tables = container.find_elements(By.CLASS_NAME, 'ba-table')
-
-                # Get the data for hitting and pitching statistics leaderboards
-                data_tables = ba_tables[0:2]
-
-                i = 0
-
-                # For both leaderboard tables, perform the following:
-                for table in data_tables:
-
-                    # Get the outerHTML of the table elements
-                    table_html = table.get_attribute('outerHTML')
-
-                    # Use a string io buffer so HTML can be fed to pd.read_html
-                    html_buffer = io.StringIO(table_html)
-
-                    # Make table html into pandas data frame
-                    panda_table = pd.read_html(html_buffer, skiprows=[0], header=0)[0]
-                    table_df = panda_table[:-2].copy()  # Remove last two rows
-
-                    # Set the league, year, and stat type
-                    table_df['League'] = league
-                    table_df['Year'] = year
-
-                    if i == 0:
-                        table_df['Type'] = 'Hitting'
-                    else:
-                        table_df['Type'] = 'Pitching'
-
-                    # Concat the new table DataFrame to the main baseball_df 
-                    baseball_df = pd.concat([baseball_df, table_df], axis=0, ignore_index=True)
-
-                    # To change the stat type for each page and table
-                    i += 1
+                table_results.append(values)
 
 
-                # Prevent rapid requests
-                time.sleep(10)
+            # Go through results and make a new get request for each link
+            for value in table_results:
+                try:
+                    driver.get(value['link'])
+
+                    # Get the league
+                    league = value['title'].split()[-2:]
+                    league = ' '.join(league)
+
+                    # Get the year
+                    match = re.search(r'(\d{4})', value['title'])
+                    if match:
+                        year = match.group(1)
+
+                    # Get the wrapper element that holds the div
+                    wrapper = driver.find_element(By.ID, 'wrapper')
+
+                    # Get the container div that holds the statistic data tables 
+                    container = wrapper.find_element(By.XPATH, 'child::div[2]')
+                    
+                    # Get the leaderboard tables of data
+                    ba_tables = container.find_elements(By.CLASS_NAME, 'ba-table')
+
+                    # Get the data for hitting and pitching statistics leaderboards
+                    data_tables = ba_tables[0:2]
+
+                    i = 0
+
+                    # For both leaderboard tables, perform the following:
+                    for table in data_tables:
+
+                        # Get the outerHTML of the table elements
+                        table_html = table.get_attribute('outerHTML')
+
+                        # Use a string io buffer so HTML can be fed to pd.read_html
+                        html_buffer = io.StringIO(table_html)
+
+                        # Make table html into pandas data frame
+                        panda_table = pd.read_html(html_buffer, skiprows=[0], header=0)[0]
+                        table_df = panda_table[:-2].copy()  # Remove last two rows
+
+                        # Set the league, year, and stat type
+                        table_df['League'] = league
+                        table_df['Year'] = year
+
+                        if i == 0:
+                            table_df['Type'] = 'Hitting'
+                        else:
+                            table_df['Type'] = 'Pitching'
+
+                        # Concat the new table DataFrame to the main baseball_df 
+                        baseball_df = pd.concat([baseball_df, table_df], axis=0, ignore_index=True)
+
+                        # To change the stat type for each page and table
+                        i += 1
 
 
-            except Exception as e:
-                print(f"Exception: {type(e).__name__} {e}")
+                    # Prevent rapid requests
+                    time.sleep(10)
 
 
-        # Write the hitting and pitching stats to a csv file
-        baseball_df.to_csv('../csv/baseball_stats.csv', index=False)
+                except Exception as e:
+                    print(f"Exception: {type(e).__name__} {e}")
 
+
+            # Perform data cleaning
+
+            # Use the values of the 'Name' column to fill the missing values for the 'Name(s)' column
+            baseball_df['Name(s)'] = baseball_df['Name(s)'].fillna(baseball_df['Name'])
+
+            # Do the same for the 'Team' and 'Team(s) columns
+            baseball_df['Team(s)'] = baseball_df['Team(s)'].fillna(baseball_df['Team'])
+
+            # Drop the 'Name' and 'Team' columns
+            baseball_df = baseball_df.drop(columns=['Name', 'Team'])
+
+            # Rename the columns
+            baseball_df = baseball_df.rename(columns={'Name(s)': 'Player', 'Team(s)': 'Team', '#': 'Statistic_Value'})
+
+            # Remove front and end whitespace and quotations from player names
+            baseball_df['Player'] = baseball_df['Player'].str.strip().str.strip('"')
+
+            # Convert statistic values to floats
+            baseball_df['Statistic_Value'] = pd.to_numeric(baseball_df['Statistic_Value'])
+
+            baseball_df.info()
+
+            # Write the hitting and pitching stats to a csv file
+            baseball_df.to_csv('../csv/baseball_stats.csv', index=False)
+        
+        else:
+            print("Table not found")
 
     except Exception as e:
         print(f"Exception: {type(e).__name__} {e}")
