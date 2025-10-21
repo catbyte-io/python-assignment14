5d4c48ad28949b362e3d1f580bfbf7d4e4c5ceb8
catbyte-io <catbyte.io@gmail.com>
2025-10-21 15:57:46 -0600

    Add scraping and cleaning for team standings. Add program messages. Change how year and league are found.

--- webscraper/webscraper.py
+++ webscraper/webscraper.py
@@ -15,11 +15,19 @@ def main():
     try:
         driver.get('https://www.baseball-almanac.com/yearmenu.shtml')
 
+        print("Scraping data. Please wait...")
+
         # Create headers
         headers = ['League', 'Year', 'Type', 'Statistic', 'Name(s)', 'Team(s)', '#', 'Top 25']
 
         # Create baseball DataFrame
         baseball_df = pd.DataFrame(columns=headers)
+
+        # Create team standings headers
+        headers2 = ['League', 'Year', 'Team | Roster', 'W', 'L', 'T', 'WP', 'GB']
+
+        # Create team standings DataFrame
+        team_standings = pd.DataFrame(columns=headers2)
 
         # Inititalize all the main datasets
         baseball_stats = []
@@ -34,10 +42,10 @@ def main():
             # List to save table results
             table_results = []
 
-            # Access each link and click
+            # Access each link and save to dictionary
             for link in table_links:
                 values = {}
-                values['title'] = link.get_attribute('title')
+
                 values['link'] = link.get_attribute('href')
 
                 # print(values['link'])
@@ -49,15 +57,6 @@ def main():
             for value in table_results:
                 try:
                     driver.get(value['link'])
-
-                    # Get the league
-                    league = value['title'].split()[-2:]
-                    league = ' '.join(league)
-
-                    # Get the year
-                    match = re.search(r'(\d{4})', value['title'])
-                    if match:
-                        year = match.group(1)
 
                     # Get the wrapper element that holds the div
                     wrapper = driver.find_element(By.ID, 'wrapper')
@@ -75,6 +74,12 @@ def main():
 
                     # For both leaderboard tables, perform the following:
                     for table in data_tables:
+
+                        # Extract the year and league
+                        first_row = table.find_elements(By.CSS_SELECTOR, 'tr')[0]
+                        title = first_row.text
+                        year = title.split()[0]
+                        league = ' '.join(title.split()[1:3])
 
                         # Get the outerHTML of the table elements
                         table_html = table.get_attribute('outerHTML')
@@ -101,6 +106,15 @@ def main():
                         # To change the stat type for each page and table
                         i += 1
 
+                    # Get team standings
+                    s_table = ba_tables[2]
+                    s_table_html = s_table.get_attribute('outerHTML')
+                    buffer = io.StringIO(s_table_html)
+                    p_table = pd.read_html(buffer, skiprows=[0], header=0)[0]
+                    p_df = p_table[:-2].copy()
+                    p_df['League'] = league
+                    p_df['Year'] = year
+                    team_standings = pd.concat([team_standings, p_df], axis=0, ignore_index=True)
 
                     # Prevent rapid requests
                     time.sleep(2)
@@ -127,9 +141,10 @@ def main():
             # Remove front and end whitespace and quotations from player names
             baseball_df['Player'] = baseball_df['Player'].str.strip().str.strip('"')
 
-            # Clean the 'Statistics' column
+            # Clean the 'Statistics' and 'Team' column of anything but letters
             baseball_df['Statistic'] = baseball_df['Statistic'].str.replace(r'[^a-zA-Z]', '', regex=True)
-
+            baseball_df['Team'] = baseball_df['Team'].str.replace(r'[^a-zA-Z]', '', regex=True)
+            
             # Convert statistic values to floats
             baseball_df['Statistic_Value'] = pd.to_numeric(baseball_df['Statistic_Value'], errors='coerce')
             baseball_df['Statistic_Value'] = baseball_df['Statistic_Value'].fillna(value=0)
@@ -141,13 +156,38 @@ def main():
             baseball_df = baseball_df.dropna()
 
             baseball_df.info()
-            
 
-            # Write the hitting and pitching stats to a csv file
+            # Clean team standings DataFrame
+            team_standings['W'] = team_standings['W'].fillna(team_standings['Wins'])
+            team_standings['L'] = team_standings['L'].fillna(team_standings['Losses'])
+            team_standings['T'] = team_standings['T'].fillna(team_standings['Ties'])
+            team_standings['Team | Roster'] = team_standings['Team | Roster'].fillna(team_standings['Team [Click for roster]'])
+
+            # Drop columns
+            team_standings = team_standings.drop(columns=['Wins', 'Losses', 'Ties', 'Team [Click for roster]'])
+            
+            # Fix column names
+            team_standings = team_standings.rename(columns={'W':'Wins', 'L':'Losses', 'T':'Ties', 'Team | Roster':'Team'})
+
+            # Drop extra columns
+            team_standings = team_standings.drop(team_standings.columns[6:], axis=1)
+
+            # Convert to numeric
+            columns_to_convert = ['Wins', 'Losses', 'Ties']
+            team_standings[columns_to_convert] = team_standings[columns_to_convert].apply(pd.to_numeric(errors='coerce'))
+
+            # Drop any non numeric rows
+            team_standings = team_standings.dropna(subset=columns_to_convert)
+            team_standings[columns_to_convert] = team_standings[columns_to_convert].astype(int)
+
+            # Write the hitting and pitching stats and team standings to a csv file
             baseball_df.to_csv('../csv/baseball_stats.csv', index=False)
+            team_standings.to_csv('../csv/team_standings.csv', index=False)
         
         else:
             print("Table not found")
+
+        print("Done scraping data.")
 
     except Exception as e:
         print(f"Exception: {type(e).__name__} {e}")
