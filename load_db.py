daba748f251f7ef461da2e97ef05d5e4722662fe
catbyte-io <catbyte.io@gmail.com>
2025-10-21 09:25:13 -0600

    Fix file paths and typos.

--- load_db.py
+++ load_db.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def import_to_db():
 
-    df = pd.read_csv('../csv/baseball_stats.csv')
+    df = pd.read_csv('./csv/baseball_stats.csv')
 
     df.info()
 
@@ -12,20 +12,20 @@ def import_to_db():
 
     national_league_df = df[df['League'] == 'National League']
 
-    players_df = df['Player', 'Team'].drop_duplicates().reset_index(drop=True)
+    players_df = df[['Player', 'Team']].drop_duplicates().reset_index(drop=True)
 
-    stats_df = df['Type', 'Statistic'].drop_duplicates().reset_index(drop=True)
+    stats_df = df[['Type', 'Statistic']].drop_duplicates().reset_index(drop=True)
 
 
-    with sqlite3.connect('../db/baseball.db') as conn:
+    with sqlite3.connect('./db/baseball.db') as conn:
 
-        df.to_sql('baseball_stats', if_exists='replace', index=False)
+        df.to_sql('baseball_stats', conn, if_exists='replace', index=False)
 
-        players_df.to_sql('players', if_exists='replace', index=False)
-        stats_df.to_sql('statistics', if_exists='replace', index=False)
+        players_df.to_sql('players', conn, if_exists='replace', index=False)
+        stats_df.to_sql('statistics', conn, if_exists='replace', index=False)
 
-        american_league_df.to_sql('american_league', if_exists='replace', index=False)
-        national_league_df.to_sql('national_league', if_exists='replace', index=False)
+        american_league_df.to_sql('american_league', conn, if_exists='replace', index=False)
+        national_league_df.to_sql('national_league', conn, if_exists='replace', index=False)
 
 
 if __name__ == '__main__':
