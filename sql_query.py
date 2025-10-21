7a0115a3350617c9edce6f836273b0fea401ea91
catbyte-io <catbyte.io@gmail.com>
2025-10-21 09:21:08 -0600

    Print each row of query results. Add commit to save database inserts and changes.

--- sql_query.py
+++ sql_query.py
@@ -1,6 +1,4 @@
-import readline
 import sqlite3
-import sys
 
 
 # SQL query program adapted from CTD sqlcommand.py
@@ -12,34 +10,50 @@ cursor = conn.cursor()
 # Show database tables
 tables = cursor.execute("SELECT name FROM sqlite_schema WHERE type='table' ORDER BY 'name'").fetchall()
 
+# Print database tables
 print("Tables:")
 for row in tables:
     print(row[0])
 
 def main():
 
+    # List to store lines of the query
     lines = []
 
     # Get SQL queries
     while True:
         try:
+            # Program line prompt
             prompt = 'sql>' if not lines else '>>>'
             line = input(prompt)
 
+            # Exit clause
             if line.strip().lower() == "exit;":
                 break
-
+            
+            # Append SQL query lines
             lines.append(line)
 
+            # Look for the end of a command
             if line.strip().endswith(';'):
                 command = ' '.join(lines).strip()
 
             lines = []
 
             try:
+                # Execute and display the results of the command
                 cursor.execute(command)
+                results = cursor.fetchall()
+                for row in results:
+                    print(row)
 
             except sqlite3.Error as e:
+                print(e)
+
+            try:
+                # Commit any changes
+                conn.commit()
+            except sqlite3.IntegrityError as e:
                 print(e)
 
         except EOFError:
